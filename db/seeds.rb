d05a2b8a38db2f6c87bc0209cfcd612b64d39b86
brynknowles <bryn.knowles@gmail.com>
2020-09-29 15:07:19 -0400

    added another seed for cone, added require pry in run.rb and began testing if relationships are working correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Customer.destroy_all
+ShopOwner.destroy_all
+IceCreamCone.destroy_all
+
 ziggy = Customer.create(name: "Ziggy")
 mojo = Customer.create(name: "Mojo")
 thor = Customer.create(name: "Thor")
@@ -8,8 +12,9 @@ whitty = ShopOwner.create(name: "Whitty the Chicken", shop_name: "Ample Hills Cr
 pb = ShopOwner.create(name: "PB the Pig", shop_name: "Ample Hills Creamery")
 mikey = ShopOwner.create(name: "Mikey Cole", shop_name: "Mikey Likes It Ice Cream")
 
-cone1 = IceCreamCone.create(ice_cream_flavor: "Southern Hospitality", cone_type: "cake", number_of_scoops: 2, customer_id: ziggy, shop_owner_id: mikey)
-cone2 = IceCreamCone.create(ice_cream_flavor: "Brady Bunch", cone_type: "sugar", number_of_scoops: 1, customer_id: mojo, shop_owner_id: mikey)
-cone3 = IceCreamCone.create(ice_cream_flavor: "Strawberries & Cream", cone_type: "waffel", number_of_scoops: 1, customer_id: ava, shop_owner_id: whitty)
-cone4 = IceCreamCone.create(ice_cream_flavor: "Corn To Run", cone_type: "cake", number_of_scoops: 3, customer_id: thor, shop_owner_id: pb)
+cone1 = IceCreamCone.create(ice_cream_flavor: "Southern Hospitality", cone_type: "cake", number_of_scoops: 2, customer: ziggy, shop_owner: mikey)
+cone2 = IceCreamCone.create(ice_cream_flavor: "Brady Bunch", cone_type: "sugar", number_of_scoops: 1, customer: mojo, shop_owner: mikey)
+cone3 = IceCreamCone.create(ice_cream_flavor: "Strawberries & Cream", cone_type: "waffel", number_of_scoops: 1, customer: ava, shop_owner: whitty)
+cone4 = IceCreamCone.create(ice_cream_flavor: "Corn To Run", cone_type: "cake", number_of_scoops: 3, customer: thor, shop_owner: pb)
+cone5 = IceCreamCone.create(ice_cream_flavor: "Corn To Run", cone_type: "sugar", number_of_scoops: 1, customer: mojo, shop_owner: walt)
 
